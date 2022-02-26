756978899a27875eb8dba04fd3b4685c351c428e
BOTKooper <botkooper@gmail.com>
2022-02-26 21:00:26 +0100

    upd

--- index.js
+++ index.js
@@ -2,9 +2,14 @@
 import child_process from 'child_process';
 import { list } from './list.js';
 
+const INSTANCES = 3;
+const TOTAL = INSTANCES * list.length;
+let launched = 0;
 
 list.forEach((uri) => {
-    for(let i = 0; i < 5; i++) {
+    for(let i = 0; i < INSTANCES; i++) {
+        launched++;
+        console.log(`launching ${launched}/${TOTAL}`);
         child_process.exec(
             `docker run -i --rm alpine/bombardier -c 10000 -d 5h -l ${uri}`,
             (err, stdout, stderr) => {
